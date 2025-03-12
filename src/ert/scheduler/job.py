262bf4297a5eba272804f12a765199e8049b9068
larsevj <levje@equinor.com>
2025-03-12 12:34:34 +0100

    Try another shield way

--- src/ert/scheduler/job.py
+++ src/ert/scheduler/job.py
@@ -114,16 +114,14 @@ class Job:
                 await self._send(JobState.SUBMITTING)
                 submit_time = time.time()
                 try:
-                    await asyncio.shield(
-                        self.driver.submit(
-                            self.real.iens,
-                            self.real.job_script,
-                            self.real.run_arg.runpath,
-                            num_cpu=self.real.num_cpu,
-                            realization_memory=self.real.realization_memory,
-                            name=self.real.run_arg.job_name,
-                            runpath=Path(self.real.run_arg.runpath),
-                        )
+                    await self.driver.submit(
+                        self.real.iens,
+                        self.real.job_script,
+                        self.real.run_arg.runpath,
+                        num_cpu=self.real.num_cpu,
+                        realization_memory=self.real.realization_memory,
+                        name=self.real.run_arg.job_name,
+                        runpath=Path(self.real.run_arg.runpath),
                     )
                 except FailedSubmit as err:
                     await self._send(JobState.FAILED)
